544aaebcaa6b6a42a8a15b091cd728006d1586d6
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-04 17:10:19 -0600

    new line after output to make it cleaner

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	fmt.Println("Enter a numbere to play the game!")
+	fmt.Println("Enter a number greater than 1 to play the game!")
 
 	if len(os.Args) != 2 {
 		fmt.Println("Please enter a number")
@@ -18,12 +18,12 @@ func main() {
 	userInput := os.Args[1]
 
 	if userInput, err := strconv.Atoi(userInput); err != nil {
-		fmt.Printf("%v is not a number", userInput)
+		fmt.Printf("%v is not a number\n", userInput)
 	} else if userInput%2 == 0 && userInput%8 == 0 {
-		fmt.Printf("%v is an even number and is divisible by 8", userInput)
+		fmt.Printf("%v is an even number and is divisible by 8\n", userInput)
 	} else if userInput%2 == 0 {
-		fmt.Printf("%v is an even number", userInput)
+		fmt.Printf("%v is an even number\n", userInput)
 	} else if userInput%2 != 0 {
-		fmt.Printf("%v is an odd number", userInput)
+		fmt.Printf("%v is an odd number\n", userInput)
 	}
 }
