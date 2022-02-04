db9075646b92d27dab3b26d517aa6432715321f4
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-04 17:07:03 -0600

    setup- initial message checked length of cl input defined userInput to take arg from cl if case, error checking & handling else if scenarios to check even or odd input else if case for even number & divisible by 8

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 
+	fmt.Println("Enter a numbere to play the game!")
+
 	if os.Args != 2 {
-		fmt.Println
+		fmt.Println("Please enter a number")
+		return
 	}
+
 	userInput := os.Args[1]
 
-	if userInput, err
+	if userInput, err := strconv.Atoi(userInput); err != nil {
+		fmt.Printf("%v is not a number", userInput)
+	} else if userInput%2 == 0 && userInput%8 == 0 {
+		fmt.Printf("%v is an even number and is divisible by 8", userInput)
+	} else if userInput%2 == 0 {
+		fmt.Printf("%v is an even number", userInput)
+	} else if userInput%2 != 0 {
+		fmt.Printf("%v is an odd number", userInput)
+	}
 }
