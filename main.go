3ad1ea779d1aae10b07f48c3f896d9d1e282c278
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-04 17:29:01 -0600

    syntax corrections notes

--- main.go
+++ main.go
@@ -8,17 +8,18 @@ import (
 
 func main() {
 
-	fmt.Println("Enter a number greater than 1 to play the game!")
-
 	if len(os.Args) != 2 {
-		fmt.Println("Please enter a number")
+		fmt.Println("Please enter a number greater than 1")
 		return
 	}
 
-	userInput := os.Args[1]
-
-	if userInput, err := strconv.Atoi(userInput); err != nil {
-		fmt.Printf("%v is not a number\n", userInput)
+	userInt := os.Args[1]
+	//	userInput is the result of userInt converted by Atoi into an int
+	// userInt this is the user input
+	if userInput, err := strconv.Atoi(userInt); err != nil {
+		// we have userInt after the comma because we want the output message to refer to the user inputted value
+		// before the value after the decimal has been dropped.
+		fmt.Printf("%s is not a number\n", userInt)
 	} else if userInput%2 == 0 && userInput%8 == 0 {
 		fmt.Printf("%v is an even number and is divisible by 8\n", userInput)
 	} else if userInput%2 == 0 {
